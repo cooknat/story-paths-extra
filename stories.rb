37a6378ae0594f916eaf1bf8cc99d938eb0260bb
Natalie Cook <natalie.t.cook@gmail.com>
2017-10-08 21:10:35 +0000

    stuff is broken

--- stories.rb
+++ stories.rb
@@ -4,48 +4,43 @@ require_relative 'models/line'
 
 enable :sessions
 
-get '/story/start' do
+get '/story/start' do #back to the start
+ 
+end
+
+get '/story/next' do #when one of the user added links are clicked
  
 end
 
 get '/story' do
   @saved_line = session["line"]
   @saved_pos = session["pos"]
-  p @saved_line
   session["line"] = ""
   session["pos"] = ""
   erb :index
 end
 
 get '/' do
-  session["story"] = [] 
- # @initial_line = Line.new(0, "once there was a bad wolf")
+  @central_line = Line.new(0, "once there was a bad wolf")
+  session["story"] = [@central_line] 
+  session["startline"] = @central_line
   erb :index
 end
 
 post '/newline' do
-    p params.inspect
-    @newline = params["line"]
-    p @newline
-    addToStory(@newline)
-    p session["story"]
-    p session["story"].length
+    addToStory(params["line"])
     session["pos"] = params["pos"]
     session["line"] = params["line"]
-    
   redirect '/story'
-  
 end 
 
 #create a line and add it to the story array
 def addToStory(storytext)
-  newln = Line.new(0, storytext)
-  session["story"] << newln
-  #session["line"] = newln.storyline
+  pid = session["startline"].parent_id
+  session["story"] << Line.new(pid, storytext)
 end  
 
-def getParentID#needed to properly create the Line object
-end  
+
 
 def reset(line)#move the chosen link to the centre
 end  
