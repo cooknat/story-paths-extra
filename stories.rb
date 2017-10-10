c82c8207d4edc01e2bca0ca87fa70a9f5a8c582c
Natalie Cook <natalie.t.cook@gmail.com>
2017-10-10 21:47:11 +0000

    loops sorted

--- stories.rb
+++ stories.rb
@@ -9,40 +9,37 @@ get '/story/start' do #back to the start
 end
 
 get '/story/next' do #when one of the user added links are clicked
-  session["pos"].clear
  
 end
 
 get '/story' do
-  @saved_line = session["line"]
-  @saved_pos = session["pos"]
-  session["line"] = ""
+  session["currentpage"] = session['story'].select do |line|
+    line.parent_id == session["startline"].id
+  end 
+  #session["currentpage"] = tempArr.sort_by{ |el| el.position }
   erb :index
 end
 
 get '/' do
-  @central_line = Line.new(0, "centre", "once there was a bad wolf")
-  session["story"] = [@central_line] 
-  session["startline"] = @central_line
-  session["pos"] = ["ignore"]
+  @startLine ||= Line.new(0, "centre", "once there was a bad wolf")
+  session["story"] ||= [@startLine]
+  session["startline"] = @startLine
+  session["currentpage"] = []
   erb :index
 end
 
 post '/newline' do
-    addToStory(params["line"], params["pos"])
-    session["pos"] << params["pos"]
-    #session["line"] = params["line"]
-    p session["story"]
+  addToStory(params["line"], params["pos"])
   redirect '/story'
 end 
+
+  
 
 #create a line and add it to the story array
 def addToStory(storytext, pos)
   pid = session["startline"].id
   session["story"] <<  Line.new(pid, pos, storytext)
 end  
-
-
 
 def reset(line)#move the chosen link to the centre
 end  
