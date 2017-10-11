d3b8f5750237bbfe05d4aaaa3cb1c07d31ec62ec
Natalie Cook <natalie.t.cook@gmail.com>
2017-10-11 08:57:55 +0000

    links working

--- stories.rb
+++ stories.rb
@@ -4,25 +4,23 @@ require_relative 'models/line'
 
 enable :sessions
 
-get '/story/start' do #back to the start
- 
-end
-
-get '/story/next' do #when one of the user added links are clicked
- 
-end
-
 get '/story' do
   session["currentpage"] = session['story'].select do |line|
     line.parent_id == session["startline"].id
   end 
-  #session["currentpage"] = tempArr.sort_by{ |el| el.position }
   erb :index
 end
 
+get '/:line' do #when one of the user added links or back to the start are clicked
+ new_start = session["story"].detect { |l| l.storyline == params[:line]} 
+ session["startline"] = new_start
+ session["currentpage"] = []
+ erb :index
+end
+
 get '/' do
-  @startLine ||= Line.new(0, "centre", "once there was a bad wolf")
-  session["story"] ||= [@startLine]
+  @startLine = Line.new(0, "centre", "once there was a bad wolf")
+  session["story"] = [@startLine]
   session["startline"] = @startLine
   session["currentpage"] = []
   erb :index
@@ -33,13 +31,10 @@ post '/newline' do
   redirect '/story'
 end 
 
-  
-
 #create a line and add it to the story array
 def addToStory(storytext, pos)
   pid = session["startline"].id
   session["story"] <<  Line.new(pid, pos, storytext)
+  p session["story"]
 end  
 
-def reset(line)#move the chosen link to the centre
-end  
